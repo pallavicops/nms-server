814ecc8889634a338207eca654b3cbeee3b13440
Pallavi Kumari <75251279+pallavicops@users.noreply.github.com>
2023-01-29 14:12:27 +0530

    initial

--- api/branches.php
+++ api/branches.php
@@ -6,10 +6,12 @@ $sql = "SELECT * FROM branchs ";
 $result= mysqli_query($conn,$sql) or die("Queary faild");
 
 if(mysqli_num_rows($result)>0){
+    http_response_code(200);
     $output =mysqli_fetch_all($result,MYSQLI_ASSOC);
     echo json_encode($output);
 }
 else{
+    http_response_code(400);
     echo json_encode(array('message' => 'No record found','Status'=>'fales'));
 }
 ?>
